c5e12b8714f89090bb2c10e40b76bd746e04382c
petronous <48959829+Petronous@users.noreply.github.com>
2020-08-17 17:48:55 +0200

    changed to rainfall

--- script.js
+++ script.js
@@ -1,26 +1,85 @@
+function clamp(min, value, max) {
+  if (value < min) return min;
+  if (value > max) return max;
+  return value;
+}
+
+function blendY(x,y, dr, cw, j){
+  yy = Math.floor(y);
+  i1 = (x + (yy+0)*cw);
+  i2 = (x + (yy+1)*cw);
+  part = y - yy;
+  if (y <= 3) {
+    y = y;
+  }
+  return dr[i1*4 + j]*part + dr[i2*4 + j]*(1-part)
+}
+
+var drops = [];
+var speeds= [];
+
 function update() {
   console.log("update")
   var canvas = document.getElementById('canvas');
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  openFullscreen(canvas)
+  //openFullscreen(canvas)
   var ctx = canvas.getContext('2d');
   var canvasWidth = canvas.width;
   var canvasHeight = canvas.height;
-  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+  //ctx.clearRect(0, 0, canvasWidth, canvasHeight);
   var id = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
-  var pixels = id.data;
-  for (var i = 0; i < pixels.length; i++) {
-    var r = Math.floor(Math.random() * 256);
-    var g = Math.floor(Math.random() * 256);
-    var b = Math.floor(Math.random() * 256);
-    pixels[i    ] = r;
-    pixels[i + 1] = g;
-    pixels[i + 2] = b;
-    pixels[i + 3] = 255;
+  if (drops.length == canvasWidth*canvasHeight*4) {
+    var pixels = drops.slice();
+    var dropsref=drops;
   }
-  ctx.putImageData(id, 0, 0);
-  return i
+  else {
+    var pixels = id.data;
+    var dropsref=id.data;
+  }
+  if (speeds.length == 0) {
+    for (var i = 0; i < canvasWidth; i++) {
+      speeds.push(Math.random()*3 + 1);
+    }
+  }
+  for (var i = 0; i < canvasWidth; i++) {
+    if (Math.random() > 0.99) speeds[i] = (Math.random()*4-1);
+  }
+  for (var i = canvasHeight*canvasWidth*4 - 4; i >= canvasWidth*4*4; i -= 4) {
+    //console.log(i)
+    left = Math.random()/10 - 0.05;
+    right= Math.random()/10 - 0.05;
+    x = Math.floor(i/4) % canvasWidth;
+    y = Math.floor(i/4  / canvasWidth);
+
+    for (var j = 0; j < 4; j++) {
+      if (true) {
+        left = 0;
+        right= 0;
+      }
+      pixels[i + j] = clamp(0, Math.floor((  blendY(x,y-speeds[x], dropsref, canvasWidth, j)   +    left*dropsref[i - 4 - canvasWidth*4 + j] + right*dropsref[i + 4 - canvasWidth*4 + j])/(1)), 255);
+    }
+  }
+  for (var i = 0; i < canvasWidth*4*4; i+=4) {
+    if (i == 0) console.log(pixels[i+2])
+    if (pixels[i+2] == 0 && Math.random() > 0.95){
+      var r = Math.floor(Math.random() * 255);
+      pixels[i    ] = Math.max(0, r - 20);
+      pixels[i + 1] = r;
+      pixels[i + 2] = Math.min(255, r + 20);
+      pixels[i + 3] = 255;
+    } else {
+      for(let j = 0; j < 3; j++) {
+        pixels[i + j] = clamp(0, pixels[i+j] - 5, 255);
+      }
+    }
+  }
+  nid = new ImageData(pixels, canvasWidth, canvasHeight);
+  console.log(nid.data[0]);
+  console.log(pixels[0]);
+  drops = pixels;
+  ctx.putImageData(nid, 0, 0);
+  return i;
 }
 
 function openFullscreen(elem) {
---
+++ script2.js
@@ -0,0 +1,36 @@
+function update() {
+  console.log("update")
+  var canvas = document.getElementById('canvas');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  openFullscreen(canvas)
+  var ctx = canvas.getContext('2d');
+  var canvasWidth = canvas.width;
+  var canvasHeight = canvas.height;
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+  var id = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+  var pixels = id.data;
+  for (var i = 0; i < canvasWidth*canvasHeight*4; i+=4) {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    pixels[i    ] = r;
+    pixels[i + 1] = g;
+    pixels[i + 2] = b;
+    pixels[i + 3] = 255;
+  }
+  ctx.putImageData(id, 0, 0);
+  return i
+}
+
+function openFullscreen(elem) {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
